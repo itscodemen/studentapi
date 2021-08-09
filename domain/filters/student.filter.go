da1e7b1117fdd88a644502a296da674d341f5808
Akshay <akshaybmt@binmile.com>
2021-08-09 13:45:15 +0530

    Added Custom Filtering Feature for Data Fields during GET

--- domain/filters/student.filter.go
+++ domain/filters/student.filter.go
@@ -10,6 +10,10 @@ import (
 type StudentFilter struct {
 	SortField *string `form:"sort_field"`
 	SortDest  *string `form:"sort_dir"`
+	Search    *string `form:"search"`
+	Name      *string `form:"name"`
+	Email     *string `form:"email"`
+	Phone     *string `form:"phone"`
 }
 
 func (sf *StudentFilter) GetSortField() string {
@@ -20,12 +24,34 @@ func (sf *StudentFilter) GetSortField() string {
 	return *sf.SortField
 }
 
+func (sf *StudentFilter) GetSearchValue() string {
+	if sf.Search == nil {
+		return ""
+	}
+
+	return *sf.Search
+}
+
 func (sf *StudentFilter) Scope(db *gorm.DB) *gorm.DB {
 	if sf.SortField != nil {
 		if sf.SortDest != nil {
-			db = db.Order(fmt.Sprintf("%s %s", sf.GetSortField(), *sf.SortDest))
+			db = db.Debug().Order(fmt.Sprintf("%s %s", sf.GetSortField(), *sf.SortDest))
 		}
 	}
+
+	if sf.Name != nil {
+		db = db.Where("name LIKE ?", "%"+*sf.Name+"%")
+	}
+	if sf.Email != nil {
+		db = db.Where("email LIKE ?", "%"+*sf.Email+"%")
+	}
+	if sf.Phone != nil {
+		db = db.Where("name LIKE ?", "%"+*sf.Phone+"%")
+	}
+	if sf.Search != nil {
+		db = db.Debug().Where(fmt.Sprintf("name LIKE %s OR email LIKE %s OR phone LIKE %s", "'%"+*sf.Search+"%'", "'%"+*sf.Search+"%'", "'%"+*sf.Search+"%'"))
+	}
+
 	return db
 }
 
